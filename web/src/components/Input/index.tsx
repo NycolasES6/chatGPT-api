abc676b0bb075bef13123bef0c75820dd3d84cc0
NycolasES6 <nycolasonrails@gmail.com>
2023-02-01 17:19:54 -0300

    Finished web

--- web/src/components/Input/index.tsx
+++ web/src/components/Input/index.tsx
@@ -1,24 +1,41 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
-import { Container, InputQuestion, Button } from './styles';
+import {OutPutText, OutPut, Form, Container, InputQuestion, Button } from "./styles";
 
 const Input: React.FC = () => {
-  const [value, setValue] = useState(String)
+  const [value, setValue] = useState(String);
+  const [outText, setOutText] = useState(String);
 
-  async function handleSubmit(e: { preventDefault: () => void; }){
+  async function handleSubmit(e: { preventDefault: () => void }) {
     e.preventDefault();
-    const question = await axios.post("http://localhost:3003/api",{
-      question: value
-    })
-
-    console.log(question.data.response)
+    setValue("");
+    const question = await axios.post("http://localhost:3003/api", {
+      question: value,
+    });
+    
+    setOutText(question.data.response)
   }
 
-  return <Container onSubmit={handleSubmit}>
-    <InputQuestion value={value} type="text" onChange={(e)=>setValue(e.target.value)} />
-    <Button type='submit'>Enviar</Button>
-  </Container>;
-}
+  return (
+    <Container>
+      <OutPut>
+      <OutPutText>
+        {
+          outText
+        }
+      </OutPutText>
+      </OutPut>
+      <Form onSubmit={handleSubmit}>
+        <InputQuestion
+          value={value}
+          type="text"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <Button type="submit">Enviar</Button>
+      </Form>
+    </Container>
+  );
+};
 
 export default Input;
--- web/src/components/Input/styles.ts
+++ web/src/components/Input/styles.ts
@@ -1,26 +1,68 @@
 import styled from "styled-components";
 
-export const Container = styled.form`
+export const Container = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 5px;
+`;
+
+export const OutPut = styled.div`
+  height: calc(100vh - 70px);
+  width: 100%;
+  background-color: var(--grey-800);
+  border-radius: 5px;
+  margin-bottom: 5px;
+  padding: 0 10px;
+  overflow-y: scroll;
+  overflow-x: hidden;
+
+  ::-webkit-scrollbar {
+    background-color: var(--grey-700);
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: var(--grey-500);
+    border-radius: 3px;
+  }
+  ::-webkit-scrollbar-button {
+    background-color: #fff;
+  }
+`;
+
+export const OutPutText = styled.p`
+  font-size: 20px;
+  font-weight: medium;
+  color: var(--white);
+`;
+
+export const Form = styled.form`
   width: 100%;
   height: 50px;
-  padding: 5px;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
 export const InputQuestion = styled.input`
-  width:100%;
+  width: 100%;
   height: 100%;
   border-radius: 3px;
-  margin-right:3px;
+  margin-right: 3px;
+  outline: none;
+  font-size: 13pt;
 `;
 
 export const Button = styled.button`
   width: 80px;
   height: 100%;
   border-radius: 3px;
-  :hover{
+  transition: 0.3s;
+  font-size: 13pt;
+  font-weight: 500;
+  :hover {
     cursor: pointer;
+    background: var(--discover-mid);
+    color: var(--white);
   }
 `;
