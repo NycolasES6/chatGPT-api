fe0d281f7ed07e0262d4102f9dbee32c7ceb541a
NycolasES6 <nycolasonrails@gmail.com>
2023-02-01 17:18:02 -0300

    finished server

--- server/src/routes/api.routes.ts
+++ server/src/routes/api.routes.ts
@@ -15,7 +15,7 @@ apiRoutes.post('/', async(req:Request, res:Response)=>{
     model: "text-davinci-003",
     prompt: `${question}`,
     temperature: 0.7,
-    max_tokens: 256,
+    max_tokens: 1000,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
